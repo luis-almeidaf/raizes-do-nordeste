4d5282c277260e5d3252c89ce365a3acdc809355
Luis <luis.almeida.faria@gmail.com>
2026-05-27 21:00:35 -0300

    fix: adiciona validador da request para pagamentos

--- src/RaizesDoNordeste.Application/Features/Pagamentos/Commands/ProcessarPagamento/ProcessarPagamentoValidator.cs
+++ src/RaizesDoNordeste.Application/Features/Pagamentos/Commands/ProcessarPagamento/ProcessarPagamentoValidator.cs
@@ -1,6 +1,15 @@
+using FluentValidation;
+using RaizesDoNordeste.Exceptions;
+
 namespace RaizesDoNordeste.Application.Features.Pagamentos.Commands.ProcessarPagamento;
 
-public class ProcessarPagamentoValidator
+public class ProcessarPagamentoValidator : AbstractValidator<ProcessarPagamentoCommand>
 {
-    
+    public ProcessarPagamentoValidator()
+    {
+        RuleFor(pagamento => pagamento.StatusPagamento).NotEmpty()
+            .WithMessage(MensagensDeErro.STATUS_DO_PAGAMENTO_VAZIO);
+        RuleFor(pagamento => pagamento.StatusPagamento).IsInEnum()
+            .WithMessage(MensagensDeErro.STATUS_DO_PAGAMENTO_INVALIDO);
+    }
 }
